bfa625e4fd5c9db135fa4f1d1e21ea79d3e11679
Alex Gagescu <alex@badass.gagescu.lab>
2018-09-21 17:00:54 -0400

    Running w/ steps

--- AG-TEST-PIPELINE-2-qa.groovy
+++ AG-TEST-PIPELINE-2-qa.groovy
@@ -1,68 +1,79 @@
 pipeline {
-  agent { label 'jenkins-slave-selenium' }
-  
-  
-  environment {
-    
-    JENKVER = "0.5"
-  }
+    // step by step
 
-  stages {
-    stage("Prepare") {
-      environment {
-        BAR = "STAGE"
+    agent { label 'jenkins-slave-selenium' }
+     
+    environment {
+        JENKVER = "0.5"
       }
-      steps {
-        cleanWs()
-          
-        nvm('v6.2.0') {
-	    	    sh 'env'
-	    }
-	        
-        dir("video-player"){
-            git branch: 'develop', credentialsId: '7c585d84-10e0-43bb-b62b-641dcf4076ad', url: 'git@github.com:mlgdev/video-player.git'
+
+    stages {
+        stage("Prepare") {
+          steps {
+            cleanWs()
+            nvm('v6.2.0') {
+    	    	    sh 'env'
+              }
+    	        
+            dir("video-player"){
+                git branch: 'develop', credentialsId: '7c585d84-10e0-43bb-b62b-641dcf4076ad', url: 'git@github.com:mlgdev/video-player.git'
+            }
+    		
+      	    dir("mlg-qa"){
+                  git branch: 'develop', credentialsId: '7c585d84-10e0-43bb-b62b-641dcf4076ad', url: 'git@github.com:mlgdev/mlg-qa.git'
+    		    }
+          }
         }
-		
-	    dir("mlg-qa"){
-            git branch: 'develop', credentialsId: '7c585d84-10e0-43bb-b62b-641dcf4076ad', url: 'git@github.com:mlgdev/mlg-qa.git'
-		}  
-		
-		sh 'ls -saltr'    
-        sh 'echo $HOSTNAME'    
-        sh 'npm install nightwatch --prefix $WORKSPACE/mlg-qa/player-func-tests'
-        sh 'npm install --prefix $WORKSPACE/video-player/player-service'
-        sh 'npm install --prefix $WORKSPACE/mlg-qa/player-func-tests'
+
+        stage("Build"){
       
-        sh 'nohup npm run --prefix $WORKSPACE/video-player/player-service/ start-local >> player-service.log &'
-        sh 'sleep 30'
-        sh 'npm run --prefix $WORKSPACE/mlg-qa/player-func-tests jenkins-test'
-        //sh 'npm stop --prefix $WORKSPACE/video-player/player-service/ start-local'
-        }
-    }
-  }
+          steps {
+            sh 'npm install nightwatch --prefix $WORKSPACE/mlg-qa/player-func-tests'
+            sh 'npm install --prefix $WORKSPACE/video-player/player-service'
+            sh 'npm install --prefix $WORKSPACE/mlg-qa/player-func-tests' 
+            }  
+          }
 
-  post {
-    always {
-        junit 'mlg-qa/player-func-tests/reports/**/*.xml'
-        //publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: 'video-player/player-service/tests/reports/unit/server/html', reportFiles: 'Server-Test-Results.html', reportName: 'HTML Report', reportTitles: ''])
-        //publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: 'video-player/player-service/tests/reports/unit/client/coverage', reportFiles: 'index.html', reportName: 'HTML Report', reportTitles: ''])
+        stage("App Start"){
+          steps{
+          sh 'nohup npm run --prefix $WORKSPACE/video-player/player-service/ start-local >> player-service.log &'
+          sh 'sleep 30'
+            }
+          }
 
-        // slackSend channel: 'engineering-alerts', color: 'good', message: 'Finished ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)'
-        echo 'Jenkins Job finished'
-        //deleteDir() /* clean up our workspace */
-   	}
-    success {
-        echo 'I succeeeded!'
-    	}
-    unstable {
-        echo 'I am unstable :/'
-   	 	}
-    failure {
-        echo 'I failed :('
-    	}
-    changed {
-        echo 'Things were different before...'
-    	}
-	}
+        stage("Testing"){
+          steps{
+             sh 'npm run --prefix $WORKSPACE/mlg-qa/player-func-tests jenkins-test'
+            //sh 'npm stop --prefix $WORKSPACE/video-player/player-service/ start-local'
+            }
+          }
+      }
 
+    post {
+      always {
+          junit 'mlg-qa/player-func-tests/reports/**/*.xml'
+          //publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: 'video-player/player-service/tests/reports/unit/server/html', reportFiles: 'Server-Test-Results.html', reportName: 'HTML Report', reportTitles: ''])
+          //publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: 'video-player/player-service/tests/reports/unit/client/coverage', reportFiles: 'index.html', reportName: 'HTML Report', reportTitles: ''])
+          echo 'Jenkins Job finished'
+          
+          //deleteDir() /* clean up our workspace */
+     	  }
+
+      success {
+          //slackSend channel: 'engineering-alerts', color: 'good', message: 'Finished ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)'
+          echo 'Success - Yeah, baby, yeah!'
+      	}
+
+      unstable {
+          echo 'Unstable - I hope this is part of the unfreezing process :/'
+     	 	}
+
+      failure {
+          echo "Failed - Mr. Bigglesworth gets upset :("
+      	}
+
+      changed {
+          echo 'Changed - If you want us to have a relationship, you have to get it into your head that times have changed!'
+      	}
+      }
 }
